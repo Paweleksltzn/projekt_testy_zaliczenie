937c39c8d02c85567baddb12fa8bdd1f2d14d380
Paweł <pawelantolak@interia.eu>
2022-02-20 09:30:53 +0100

    fix: removed mock data from gameData

--- data/gameData.js
+++ data/gameData.js
@@ -1,10 +1,3 @@
 exports.scores = [];
 
-exports.currentGames = [
-    {
-        idSesji: 1645344820277,
-        liczbaProb: 0,
-        czasRozpoczeciaGry: 1645343991083,
-        liczba: 67
-    }
-];
+exports.currentGames = [];
